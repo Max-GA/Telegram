b6583503e2557b15fd4d07c0f275254b207682c8
Max-GA <33633761+Max-GA@users.noreply.github.com>
2017-11-27 18:21:21 +0100

    Version 0.3

--- Profiles.py
+++ Profiles.py
@@ -9,6 +9,70 @@ class Contacts(object):
             DATA['contacts'] = []
             self.setJson(DATA)
 
+    def shortCourse(self, id, course, main=True):
+        if course == "Englisch" or course == "englisch" or course == "English" or course == "english":
+            return "E"
+        elif course == "Mathe" or course == "mathe" or course == "Maths" or course == "maths":
+            return "M"
+        elif course == "Deutsch" or course == "deutsch" or course == "German" or course == "german":
+            return "D"
+        elif course == "Französisch" or course == "französisch" or course == "French" or course == "french":
+            return "F"
+        elif course == "Erdkunde" or course == "erdkunde" or course == "Geography" or course == "geography":
+            if int(self.getGrade(id)) >= 11:
+                if main:
+                    return "Ek"
+                else:
+                    return "Eso"
+            else:
+                return "Ek"
+        elif course == "Sozialkunde" or course == "sozialkunde" or course == "Politics" or course == "politics":
+            if int(self.getGrade(id)) >= 11:
+                if main:
+                    return "Sk"
+                else:
+                    return "Eso"
+            else:
+                return "Sk"
+        elif course == "Chemie" or course == "chemie" or course == "Chemistry" or course == "chemistry":
+            if int(self.getGrade(id)) >= 7:
+                return "Ch"
+            else:
+                return "Nw"
+        elif course == "Ethik" or course == "ethik" or course == "Ethics" or course == "ethics":
+            return "Et"
+        elif course == "Religion" or course == "Religion":
+            return "R"
+        elif course == "Biologie" or course == "biologie" or course == "Biology" or course == "biology":
+            if int(self.getGrade(id)) >= 7:
+                return "Bi"
+            else:
+                return "Nw"
+        elif course == "Physik" or course == "physik" or course == "Physics" or course == "physics":
+            if int(self.getGrade(id)) >= 7:
+                return "ph"
+            else:
+                return "Nw"
+        elif course == "Musik" or course == "musik" or course == "Music" or course == "music":
+            return "Mu"
+        elif course == "Kunst" or course == "kunst" or course == "Art" or course == "art":
+            return "Bk"
+        elif course == "Latein" or course == "latein" or course == "Latin" or course == "latin":
+            return "L"
+        elif course == "Geschichte" or course == "geschichte" or course == "History" or course == "history":
+            return "G"
+        elif course == "Informatik" or course == "informatik" or course == "Computer Science" or course == "computer science":
+            if main:
+                return "Inf"
+            else:
+                return "In"
+        elif course == "Sport" or course == "sport" or course == "Sports" or course == "sports":
+            return "Sp"
+        elif course == "Eso" or course == "eso":
+            return "Eso"
+        else:
+            return False
+
     def getJson(self):
         with open("Contacts.json") as json_file:
             data = json.load(json_file)
@@ -23,10 +87,79 @@ class Contacts(object):
         DATA['contacts'].append(
             {
                 'name': name,
-                'id': id
+                'id': id,
+                'school': False
             }
         )
         self.setJson(DATA)
+
+    def setGrade(self, id, grade):
+        DATA = self.getJson()
+        new_DATA = {}
+        new_DATA['contacts'] = []
+        for contact in DATA['contacts']:
+            if contact['id'] == id:
+                contact['school'] = True
+                contact['grade'] = grade
+            new_DATA['contacts'].append(contact)
+        self.setJson(new_DATA)
+    def getGrade(self, id):
+        DATA = self.getJson()
+        for contact in DATA['contacts']:
+            if contact['id'] == id:
+                return contact['grade']
+
+    def getCourses(self, id):
+        DATA = self.getJson()
+        result = []
+        for contact in DATA['contacts']:
+            if contact['id'] == id:
+                for main in range(0, len(contact['main_courses'])):
+                    result.append(contact['main_courses']['{}_course'.format(main)].upper())
+                for rest in range(0, len(contact['other_courses'])):
+                    result.append(contact['other_courses']['{}_course'.format(rest)].upper())
+        return result
+
+    def setMainCourses(self, id, courses):
+        DATA = self.getJson()
+        new_DATA = {}
+        new_DATA['contacts'] = []
+        for contact in DATA['contacts']:
+            if contact['id'] == id:
+                contact['main_courses'] = {}
+                for course_num in range(0, len(courses)):
+                    contact['main_courses']['{}_course'.format(str(course_num))] = courses[course_num]
+            new_DATA['contacts'].append(contact)
+        self.setJson(new_DATA)
+
+    def setOtherCourses(self, id, courses):
+        DATA = self.getJson()
+        new_DATA = {}
+        new_DATA['contacts'] = []
+        for contact in DATA['contacts']:
+            if contact['id'] == id:
+                contact['other_courses'] = {}
+                for course_num in range(0, len(courses)):
+                    contact['other_courses']['{}_course'.format(str(course_num))] = courses[course_num]
+            new_DATA['contacts'].append(contact)
+        self.setJson(new_DATA)
+
+
+    def getSchool(self, id):
+        DATA = self.getJson()
+        for contact in DATA['contacts']:
+            if contact['id'] == id:
+                return contact['school']
+
+    def resetSchool(self, id):
+        DATA = self.getJson()
+        new_DATA = {}
+        new_DATA['contacts'] = []
+        for contact in DATA['contacts']:
+            if contact['id'] == id:
+                contact['school'] = False
+            new_DATA['contacts'].append(contact)
+        self.setJson(new_DATA)
 
     def getName(self, id):
         DATA = self.getJson()
--- PyCHT_Bot.py
+++ PyCHT_Bot.py
@@ -2,6 +2,7 @@ import json
 import bs4 as bs
 import urllib.request
 import requests
+from lxml import html
 import time
 import threading
 import Profiles
@@ -11,6 +12,7 @@ class Bot(object):
     def __init__(self):
         self.Contacts = Profiles.Contacts()
         self.URL = "https://api.telegram.org/bot433782927:AAGhtZEFd6mx59OAHMiGROlV7R4_xgxILZI/"
+        self.School_URL = "http://leibniz-pirmasens.de/vertretungsplan/vertretungklasse.html"
         self.Conversations = self.Contacts.getIDs()
         self.Handling_IDs = []
         self.Last_Result = None
@@ -41,6 +43,8 @@ class Bot(object):
                 self.commands(Chat_ID)
             elif self.getText(Result) == "/time":
                 self.time(Chat_ID)
+            elif self.getText(Result) == "/school":
+                self.school(Chat_ID)
             else:
                 self.sendMessage(Chat_ID, "Sorry! I couldn't understand you :(")
         else:
@@ -52,6 +56,10 @@ class Bot(object):
                 if new_Update_ID != Update_ID and self.getID(new_Result) == Chat_ID:
                     Name = self.getText(new_Result)
                     self.sendMessage(Chat_ID, "Nice to meet you , {}!\nYou can see what I can do by typing /help or /commands\nThank you for talking with me :)".format(Name))
+
+                    if Name == "Julia" or Name == "julia":
+                        self.sendMessage(Chat_ID, "Btw, I think my father loves you\nHe is always talking and thinking about someone named 'Julia' ...\nBut I'm just a robot , what do I know ;)")
+
                     self.Update_ID = new_Update_ID
 
                     self.Contacts.newContact(Name, Chat_ID)
@@ -69,7 +77,6 @@ class Bot(object):
         """
         self.sendMessage(Chat_ID, text)
 
-
     def time(self, Chat_ID):
         time_now = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
         if 12 > int(time_now.split(" ")[1].split(":")[0]) >= 0:
@@ -79,11 +86,202 @@ class Bot(object):
         elif 24 > int(time_now.split(" ")[1].split(":")[0]) >= 18:
             self.sendMessage(Chat_ID, "Good night , it is the " + time_now.split(" ")[0] + " and the time is : " + time_now.split(" ")[1])
 
+    def school(self, Chat_ID):
+        if not self.Contacts.getSchool(Chat_ID):
+            Result = self.lastUpdate(self.getUpdates_Json())
+            Update_ID = self.getUpdateID(Result)
 
-    def getUpdates_Json(self):
-        args = {"timeout": 100, "offset": None}
+            self.sendMessage(Chat_ID, "Okay, before you can use this command I have to know somethings about you and your school!")
+            self.sendMessage(Chat_ID, "At first I'm going to collect some information about your sujects and which grade your in.\nPlease answer the questions correctly\nIf you want to stop this setup , type /stop")
+            self.sendMessage(Chat_ID, "So Please , which grade are you in ?\nA number would be great :)")
+
+            grade_done = False
+            first_courses = False
+            rest_courses = False
+
+            while not grade_done:
+                new_Result = self.lastUpdate(self.getUpdates_Json())
+                new_Update_ID = self.getUpdateID(new_Result)
+                if new_Update_ID != Update_ID and self.getID(new_Result) == Chat_ID:
+                    grade = self.getText(new_Result)
+                    if grade != "/stop":
+                        self.sendMessage(Chat_ID, "Okay {}, you are in the {} grade".format(self.Contacts.getName(Chat_ID), grade))
+                        self.Contacts.setGrade(Chat_ID, grade)
+                        Update_ID = new_Update_ID
+                        grade_done = True
+                    else:
+                        self.sendMessage(Chat_ID, "You stopped the setup\nSorry to bother you")
+                        self.Contacts.resetSchool(Chat_ID)
+                        self.Update_ID = new_Update_ID
+                        return False
+            if int(self.Contacts.getGrade(Chat_ID)) > 10:
+                self.sendMessage(Chat_ID, "Now I need your Advanced Courses\nPlease write them in the following format:\n\nCourse1 - Number(if important)\nCourse2\nCourse3 - Number(if important)\n...")
+            else:
+                self.sendMessage(Chat_ID, "Now I need our Main Courses\nPlease write them in the following format:\n\nCourse1 - Number(if important)\nCourse2\nCourse3 - Number(if important)\n...")
+            while not first_courses:
+                new_Result = self.lastUpdate(self.getUpdates_Json())
+                new_Update_ID = self.getUpdateID(new_Result)
+                if new_Update_ID != Update_ID and self.getID(new_Result) == Chat_ID:
+                    courses = self.getText(new_Result)
+                    if courses != "/stop":
+                        if "\n" in courses:
+                            self.sendMessage(Chat_ID, "Thanks! , Your courses are :")
+                            temp = []
+                            for course in courses.split("\n"):
+                                if len(course.split("-")) > 1:
+                                    if self.Contacts.shortCourse(Chat_ID, course.split("-")[0].replace(" ","")) != False:
+                                        temp.append(self.Contacts.shortCourse(Chat_ID, course.split("-")[0].replace(" ",""))+course.split("-")[1].replace(" ",""))
+                                    else:
+                                        self.sendMessage(Chat_ID, "You inserted a course that is not in my database, please use only common subjects\nMaybe you also did a Typo , so please be careful when typing your courses\nI'm sorry but you have to do it again :(")
+                                        self.Contacts.resetSchool(Chat_ID)
+                                        self.Update_ID = new_Update_ID
+                                        return False
+                                else:
+                                    if self.Contacts.shortCourse(Chat_ID, course) != False:
+                                        temp.append(self.Contacts.shortCourse(Chat_ID, course))
+                                    else:
+                                        self.sendMessage(Chat_ID, "You inserted a course that is not in my database, please use only common subjects\nMaybe you also did a Typo , so please be careful when typing your courses\nI'm sorry but you have to do it again :(")
+                                        self.Contacts.resetSchool(Chat_ID)
+                                        self.Update_ID = new_Update_ID
+                                        return False
+                                self.sendMessage(Chat_ID, course)
+                            self.Contacts.setMainCourses(Chat_ID, temp)
+                            del temp
+                            Update_ID = new_Update_ID
+                            first_courses = True
+                        else:
+                            self.sendMessage(Chat_ID, "Wrong format ... Please try again")
+                            Update_ID = new_Update_ID
+                    else:
+                        self.sendMessage(Chat_ID, "You stopped the setup\nSorry to bother you")
+                        self.Contacts.resetSchool(Chat_ID)
+                        self.Update_ID = new_Update_ID
+                        return False
+            self.sendMessage(Chat_ID, "The last things I need are your other courses\nSo now please again, tell me your different courses in the following format :\n\nCourse1 - Number(if important)\nCourse2\nCourse3 - Number(if important)\n...")
+            while not rest_courses:
+                new_Result = self.lastUpdate(self.getUpdates_Json())
+                new_Update_ID = self.getUpdateID(new_Result)
+                if new_Update_ID != Update_ID and self.getID(new_Result) == Chat_ID:
+                    courses = self.getText(new_Result)
+                    if courses != "/stop":
+                        if "\n" in courses:
+                            self.sendMessage(Chat_ID, "Thanks! , Your other courses are :")
+                            temp = []
+                            for course in courses.split("\n"):
+                                if len(course.split("-")) > 1:
+                                    if self.Contacts.shortCourse(Chat_ID, course.split("-")[0].replace(" ",""), False) != False:
+                                        temp.append(self.Contacts.shortCourse(Chat_ID, course.split("-")[0].replace(" ",""))+course.split("-")[1].replace(" ",""))
+                                    else:
+                                        self.sendMessage(Chat_ID, "You inserted a course that is not in my database, please use only common subjects\nMaybe you also did a Typo , so please be careful when typing your courses\nI'm sorry but you have to do it again :(")
+                                        self.Contacts.resetSchool(Chat_ID)
+                                        self.Update_ID = new_Update_ID
+                                        return False
+                                else:
+                                    if self.Contacts.shortCourse(Chat_ID, course, False) != False:
+                                        temp.append(self.Contacts.shortCourse(Chat_ID, course))
+                                    else:
+                                        self.sendMessage(Chat_ID, "You inserted a course that is not in my database, please use only common subjects\nMaybe you also did a Typo , so please be careful when typing your courses\nI'm sorry but you have to do it again :(")
+                                        self.Contacts.resetSchool(Chat_ID)
+                                        self.Update_ID = new_Update_ID
+                                        return False
+                                self.sendMessage(Chat_ID, course)
+                            self.Contacts.setOtherCourses(Chat_ID, temp)
+                            del temp
+                            Update_ID = new_Update_ID
+                            rest_courses = True
+                    else:
+                        self.sendMessage(Chat_ID, "You stopped the setup\nSorry to bother you")
+                        self.Contacts.resetSchool(Chat_ID)
+                        self.Update_ID = new_Update_ID
+                        return False
+            self.sendMessage(Chat_ID, "Thank you , you finished the setup\nYou can use /school now to see which lessons are changed for the next day !")
+            self.Update_ID = Update_ID
+        else:
+
+            page = html.fromstring(urllib.request.urlopen(self.School_URL + "?klasse=" + self.Contacts.getGrade(Chat_ID)).read())
+            TITLE = page.xpath("//table//td")
+            DATA = page.xpath("//table//td")
+            start_var = 0
+            INFORMATION = []
+
+            # LOOP FOR MORE INFO IN TABLE , ALWAYS 7 STEPS
+            # INFO IN TEXT
+            # AUSGABE
+            if len(DATA[0]) > 5:
+                self.sendMessage(Chat_ID, "Important Information for this Day")
+                self.sendMessage(Chat_ID, str(DATA[0].text))
+                DATA.pop(DATA.index(DATA[0]))
+                start_var = 1
+
+            for i in range(0, len(DATA[start_var:]), 8):
+                lesson_num = str(DATA[i].text)
+                grade = str(DATA[i+1].text)
+                lesson_nam = str(DATA[i+2].text)
+                new_lesson = str(DATA[i+3].text)
+                by_teacher = str(DATA[i+4].text)
+                in_room = str(DATA[i+5].text)
+                inplace_of = str(DATA[i+6].text)
+                info = str(DATA[i+7].text)
+                INFORMATION.append([lesson_num, grade, lesson_nam, new_lesson, by_teacher, in_room, inplace_of, info])
+
+
+            self.sendMessage(Chat_ID, "Following information are affecting you")
+            for row in INFORMATION:
+                text = ""
+                if row[2].upper() in self.Contacts.getCourses(Chat_ID):
+                    if row[7] == "frei":
+                        text = "Your subject {} is being cancelled in the {} lesson".format(row[2], row[0])
+                    else:
+                        if row[0] == 1:
+                            text = text + """In the 1st lesson\n"""
+                        elif row[0] == 2:
+                            text = text + """In the 2nd lesson\n"""
+                        elif row[0] == 3:
+                            text = text + """In the 3rd lesson\n"""
+                        else:
+                            text = text + """In the {}th lesson\n""".format(row[0])
+
+                        text = text + "Your subject {}".format(row[2])
+                        if row[2] != row[3]:
+                            text = text + " is replaced by {}".format(row[3])
+
+                        text = text + " in room"
+
+                        if row[5] != "---":
+                            text = text + " {}".format(row[5])
+                        else:
+                            pass ### SCHAUE OB DIESER FALL ÜBERHAUPT EINTRIFFT
+
+                        if row[4] != "---":
+                            text = text + " with {} as the Teacher".format(row[4])
+
+                        if row[6] != "&nbsp;":
+                            text = text + "\nInstead of {}".format(row[6])
+
+                        text = text + "\nInformation : {}".format(row[7])
+                    self.sendMessage(Chat_ID, text)
+
+
+
+
+        """
+            In the 1st lesson\n
+            Your subject g3 is replaced by e1 in room 301 with Ve as the Teacher
+            Instead of g4
+            Information : frei
+        """
+
+    def getUpdates_Json(self, clear = False):
+        if clear:
+            args = {"timeout": 100, 'offset': self.Update_ID+1}
+        else:
+            args = {"timeout": 100, 'offset': None}
+
         DATA = requests.get(self.URL+"getUpdates", data = args)
-        return DATA.json()
+        if len(DATA.json()['result']) >= 90:
+            self.getUpdates_Json(True)
+        else:
+            return DATA.json()
 
     def lastUpdate(self, data):
         JSON_Results = data["result"]
